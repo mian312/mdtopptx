db20bc38ca3a4b0f17f4f93c7627e773810daf2b
mian312 <deymilan312@gmail.com>
2024-05-18 16:32:07 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    try:
+        with open('requirements.txt', 'r', encoding='utf-8-sig') as f:
+            return f.read().splitlines()
+    except Exception as e:
+        print(f"Error reading requirements.txt: {e}")
+        return []
+
+setup(
+    name='mdtopptx',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=read_requirements(),
+)
